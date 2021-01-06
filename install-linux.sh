d815eb6e71b40387ac96c627c6f95b8ba97fd876
deeg05 <45331856+deeg05@users.noreply.github.com>
2021-01-06 18:05:38 +0000

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -10,5 +10,7 @@ pacstrap /mnt base linux linux-firmware man man-pages neovim nano ed grub sudo
 genfstab -U /mnt >> /mnt/etc/fstab
 # Copy paper scripts from installation media
 cp -r /paper /mnt/
+# Set TERM variable since we're chrooting into system without termite
+export TERM=xterm-256color
 # Chroot into newly created installation
 arch-chroot /mnt
