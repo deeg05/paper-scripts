93907e1e703f072e3f74f0207d5ff64031f06f60
deeg05 <45331856+deeg05@users.noreply.github.com>
2021-01-06 17:51:55 +0000

    minor fixes

--- install-linux.sh
+++ install-linux.sh
@@ -9,6 +9,6 @@ pacstrap /mnt base linux linux-firmware man man-pages nvim nano ed grub sudo
 # Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 # Copy paper scripts from installation media
-cp /paper /mnt/
+cp -r /paper /mnt/
 # Chroot into newly created installation
 arch-chroot /mnt
